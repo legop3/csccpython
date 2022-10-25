39eec89f1d3b773e563eee72c52735af770b91bd
legop3 <minecratfd8@gmail.com>
2022-10-25 19:45:40 -0400

    madlib

---
+++ oct 25/madlibs.py
@@ -0,0 +1,14 @@
+# madlibs.py
+# daniel roberts
+# 10/25/2022
+# madlibs game
+
+noun = input("Please enter a noun ")
+noun2 = input("Please enter a second noun ")
+adjective = input("Please input an adjective ")
+verb = input("Please enter a past-tense verb ")
+
+print("Mary had a little",noun)
+print("it's",noun2,"was",adjective,"as snow")
+print("and everywhere that Mary",verb)
+print("the",noun,"was sure to go")
